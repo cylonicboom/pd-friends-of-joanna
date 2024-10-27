e669778a8c753ee44c586d17fd65904963f70f36
Catherine Reprobate <ginger.battlequeen@gmail.com>
2024-10-27 15:18:38 -0700

    Introduce `Toggle Eyelids` button
    aliases:  `CK_0040` / `BUTTON_EYELIDS`

--- port/src/optionsmenu.c
+++ port/src/optionsmenu.c
@@ -1648,6 +1648,7 @@ static const struct menubind menuBinds[] = {
 	{ CK_8000,   "Cycle Crouch [+]\n",  "N64 Ext 8000\n" },
 	{ CK_4000,   "Half Crouch [+]\n",   "N64 Ext 4000\n" },
 	{ CK_2000,   "Full Crouch [+]\n",   "N64 Ext 2000\n" },
+	{ CK_0040,   "Toggle Eyelids [+]\n","N64 Ext 0040\n" },
 	{ CK_ACCEPT, "UI Accept [+]\n",     "EXT UI Accept\n" },
 	{ CK_CANCEL, "UI Cancel [+]\n",     "EXT UI Cancel\n" },
 };
