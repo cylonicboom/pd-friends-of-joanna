8be552e4433d88a76f5a70795d64529af7ddd6b5
Raf <>
2025-03-29 04:19:46 +0800

    mpsetups: manage default setup + save the selected random weapons

--- src/game/mplayer/mplayer.c
+++ src/game/mplayer/mplayer.c
@@ -3915,6 +3915,23 @@ void mp0f18dec4(s32 slot)
 #endif
 }
 
+static u64 packWeaponSetRandomFilters()
+{
+	u64 packed = 0;
+	for (int i = 0; i < NUM_MPWEAPONS; ++i) {
+		packed |= g_MpWeaponSetRandomFilters[i] != 0 ? (1 << i) : 0;
+	}
+
+	return packed;
+}
+
+static void unpackWeaponSetRandomFilters(u64 packed)
+{
+	for (int i = 0; i < NUM_MPWEAPONS; ++i) {
+		g_MpWeaponSetRandomFilters[i] = (packed & (1 << i)) >> i;
+	}
+}
+
 void mpsetupfileLoadWad(struct savebuffer *buffer)
 {
 	s32 i;
@@ -3955,6 +3972,9 @@ void mpsetupfileLoadWad(struct savebuffer *buffer)
 	for (i = 0; i < ARRAYCOUNT(g_MpSetup.weapons); i++) {
 		g_MpSetup.weapons[i] = savebufferReadBits(buffer, 7);
 	}
+
+	u64 wpnRndPacked = savebufferReadBits(buffer, 64);
+	unpackWeaponSetRandomFilters(wpnRndPacked);
 
 	func0f18913c();
 
@@ -4021,6 +4041,9 @@ void mpsetupfileSaveWad(struct savebuffer *buffer)
 	for (i = 0; i < ARRAYCOUNT(g_MpSetup.weapons); i++) {
 		savebufferOr(buffer, g_MpSetup.weapons[i], 7);
 	}
+
+	u64 wpnRndPacked = packWeaponSetRandomFilters();
+	savebufferOr(buffer, wpnRndPacked, 64);
 
 	savebufferOr(buffer, g_MpSetup.timelimit, 6);
 	savebufferOr(buffer, g_MpSetup.scorelimit, 7);
