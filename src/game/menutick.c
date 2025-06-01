feb618e56112167c7a86b8a941ec3c5056a7649f
Catherine Reprobate <catherine.reprobate@pm.me>
2025-05-31 17:07:16 -0700

    add comment to menutick.c

--- src/game/menutick.c
+++ src/game/menutick.c
@@ -274,8 +274,10 @@ void menuTick(void)
 		g_IsModalMenuMode = g_MenuData.bg == 0 ? false : true;
 	}
 
+	// Logic for special return to menu sequence (ie team missions or combat simulator)
+	// that deviates from the camera swiveling behind Jo's head at her computer
+	// and going to 'Perfect Menu':
 	// Check if returning from a multiplayer match
-	// TODO: Add Team Missions awareness
 	if (g_MenuTransitionFlags > 0) {
 		if (g_Vars.lvframenum >= 4) {
 			if (g_Vars.stagenum == STAGE_CITRAINING || g_Vars.stagenum == STAGE_4MBMENU) {
