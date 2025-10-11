243ce56b32bf46185ecf5366583512636d9237e2
Catherine Reprobate <catherine.reprobate@pm.me>
2025-10-11 08:47:06 -0700

    correctly resolve chrnum for cached coop

--- src/game/chraction.c
+++ src/game/chraction.c
@@ -14247,8 +14247,10 @@ s32 chrResolveId(struct chrdata *ref, s32 id)
 			}
 			break;
 		case CHR_COOP:
-			if (ref->coopplayernum >= 0){
-				id = ref->coopplayernum;
+			if (g_Vars.coopplayers[ref->coopplayernum]
+					&& g_Vars.coopplayers[ref->coopplayernum]->prop
+					&& g_Vars.coopplayers[ref->coopplayernum]->prop->chr) {
+				id = g_Vars.coopplayers[ref->coopplayernum]->prop->chr->chrnum;
 			}
 			else if (g_Vars.coop && g_Vars.coop->prop && g_Vars.coop->prop->chr) {
 				id = g_Vars.coop->prop->chr->chrnum;
