9de745687fce9c245695c95cabd6ccde61c71af2
Catherine Reprobate <catherine.reprobate@pm.me>
2025-11-29 10:39:00 -0800

    fixup: remove extra parenth

--- run-fojo.ps1
+++ run-fojo.ps1
@@ -89,7 +89,7 @@ $DEFAULT_ARGS = @(
     "--moddir",    $(Wrap-PathIfNeeded "$($env:PD_MODDIR)/mod_gex"),
     "--moddir",    $(Wrap-PathIfNeeded "$($env:PD_MODDIR)/mod_kakariko"),
     "--moddir",    $(Wrap-PathIfNeeded "$($env:PD_MODDIR)/mod_dark_noon"),
-    "--moddir",    $(Wrap-PathIfNeeded "$($env:PD_MODDIR)/mod_goldfinger)_64"),
+    "--moddir",    $(Wrap-PathIfNeeded "$($env:PD_MODDIR)/mod_goldfinger_64"),
     "--moddir",    $(Wrap-PathIfNeeded "$($env:PD_MODDIR)/mod_fojo"),    
     "--savedir",   $(Wrap-PathIfNeeded $env:PD_SAVEDIR),                 
     "--basedir",   $(Wrap-PathIfNeeded $env:PD_BASEDIR),                 
